4ea4f5047f603b4aca1aa8ffb5931a1e4532b20b
srbibb <sarahbibb4@gmail.com>
2022-08-02 15:23:32 +0100

    Remove duplicate cells from patchy thresholding

--- src-plugins/Sarah_Mitotic_Threshold.java
+++ src-plugins/Sarah_Mitotic_Threshold.java
@@ -18,9 +18,12 @@ public class Sarah_Mitotic_Threshold implements PlugIn {
     RoiManager roiManager;
 
     public void run(String args) {
-        IJ.run("Bio-Formats Importer");// Open new file
         roiManager = new RoiManager();
+
+        IJ.run("Bio-Formats Importer");// Open new file;
         ImagePlus imp = WindowManager.getCurrentImage();
+        String filename = imp.getShortTitle();
+
         IJ.run(imp, "Split Channels", "");
         String[] channelTitles = WindowManager.getImageTitles();
         for (int i = 0; i < channelTitles.length-1; i++) {
@@ -39,14 +42,14 @@ public class Sarah_Mitotic_Threshold implements PlugIn {
 
         imp.hide();
 
-        Roi[] mitotic = findMitotic(roiManager, imp.duplicate());
+        Roi[] mitotic = findMitotic(imp.duplicate());
         Roi[] cells = thresholdCells(imp.duplicate());
 
         ArrayList<Roi> mitoticCells = thresholdMitotic(mitotic,cells);
-        saveImages(mitoticCells, originalImage);
+        saveImages(mitoticCells, originalImage, filename);
     }
 
-    private Roi[] findMitotic(RoiManager roiManager, ImagePlus ZStack){
+    private Roi[] findMitotic(ImagePlus ZStack){
         ImagePlus imp = ZStack;
         imp.setTitle("Mitotic Z-project");
         imp.show();
@@ -67,7 +70,7 @@ public class Sarah_Mitotic_Threshold implements PlugIn {
         ImagePlus imp = ZStack;
         imp.setTitle("Cell Z-project");
         imp.show();
-        roiManager.runCommand("Show None");
+        roiManager.reset();
         ImageConverter ic = new ImageConverter(imp);
         ic.convertToGray16();
         imp.updateAndDraw();
@@ -81,7 +84,6 @@ public class Sarah_Mitotic_Threshold implements PlugIn {
         ImagePlus masks = WindowManager.getCurrentImage();
         Roi[] cells = roiManager.getRoisAsArray();
         masks.close();
-        ZStack.show();
         return cells;
     }
 
@@ -90,7 +92,7 @@ public class Sarah_Mitotic_Threshold implements PlugIn {
         for (Roi mitoticCell : mitotic) {
             double[] centre = mitoticCell.getContourCentroid();
             for (Roi cell : cells) {
-                if (cell.containsPoint(centre[0], centre[1])) {
+                if (cell.containsPoint(centre[0], centre[1]) && (!mitoticCells.contains(cell))) {
                     mitoticCells.add(cell);
                 }
             }
@@ -98,16 +100,12 @@ public class Sarah_Mitotic_Threshold implements PlugIn {
         return mitoticCells;
     }
 
-    private void saveImages(ArrayList<Roi> mitoticCells, ImagePlus original) {
+    private void saveImages(ArrayList<Roi> mitoticCells, ImagePlus original, String filename) {
         //create a new folder to save files in
         original.show();
         WindowManager.getWindow("Original Z-Project");
-        //String filename = original.getShortTitle();
         String newDirectory = "D:/icm/data/cropped";
         new File(newDirectory).mkdir();
-
-        //for 100x images pixel width of 0.1095202 microns
-        double pixelToMicron = 9.130736;
 
         roiManager.reset();
         for (Roi roi : mitoticCells) {
@@ -115,26 +113,20 @@ public class Sarah_Mitotic_Threshold implements PlugIn {
         }
         roiManager.runCommand("Show All");
         for (int i=0; i < mitoticCells.size(); i++) {
-            double[] centrePoint = roiManager.getRoi(i).getContourCentroid();
-            Point pt = new Point((int) centrePoint[0], (int) centrePoint[1]);
-            double lCrop = 25*pixelToMicron;
-            original.setRoi (new Roi(pt.x - lCrop / 2, pt.y - lCrop / 2, lCrop, lCrop));
-            ImagePlus impCrop = original.crop();
-            IJ.saveAsTiff(impCrop, Paths.get(newDirectory, "Cropped" + i).toString());
+            ImagePlus impCrop = cropRoi(original, i);
+            IJ.saveAsTiff(impCrop, Paths.get(newDirectory, filename + "_" + i).toString());
             impCrop.close();
         }
     }
 
-    private String mergeChannels(String[] choicesArray) {
-        String mergeString = "";
-        for (int x = 0; x < 7; x++) {
-            if (!choicesArray[x].equals("--")) {
-                String channelString = "c" + (x + 1) + "=[" + choicesArray[x] + "] ";
-                mergeString = mergeString.concat(channelString);
-            }
-        }
-        mergeString = mergeString.concat("create keep");
-        return mergeString;
-    }
+    private ImagePlus cropRoi(ImagePlus original, int no) {
+        //for 100x images pixel width of 0.1095202 microns
+        double pixelToMicron = 9.130736;
 
+        double[] centrePoint = roiManager.getRoi(no).getContourCentroid();
+        Point pt = new Point((int) centrePoint[0], (int) centrePoint[1]);
+        double lCrop = 25*pixelToMicron;
+        original.setRoi (new Roi(pt.x - lCrop / 2, pt.y - lCrop / 2, lCrop, lCrop));
+        return original.crop();
+    }
 }
